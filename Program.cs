2843e607fad7fd2f333efe6e9f7f5128d44fe595
Petrov Nikolay <nikolay.petrov99@mail.ru>
2022-08-05 22:31:44 +0300

    dz1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое значение");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе значение");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB)
+{
+    Console.WriteLine("Первое значение"+ numberA + "больше второго значения"+ numberB);
+}
+else if (numberB>numberA){
+    Console.WriteLine("Второе значение"+ numberB + "больше первого значения"+ numberA);
+}
